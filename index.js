73dd3ef2d7a663f35761bb55cbeee50865c5a1c7
ThienStylemen <thienstylemen@gmail.com>
2020-03-21 12:43:36 +0700

    Clicking and Dragging

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const {Engine, Render, Runner, World, Bodies} = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
 const engine = Engine.create();
-const { world} = engine;
+const { world } = engine;
 
 const render = Render.create({
     element: document.body, // where we want to show
@@ -14,13 +14,19 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
+World.add(
+    world,
+    MouseConstraint.create(engine, {
+        mouse: Mouse.create(render.canvas)
+    }));
+
 //walls
 const walls = [
-    Bodies.rectangle(400,0, 800,40, {isStatic: true}),
-    Bodies.rectangle(400, 600, 800,40, {isStatic: true}),
-    Bodies.rectangle(0, 300, 40,600, {isStatic: true}),
-    Bodies.rectangle(800, 300, 40,600, {isStatic: true}),
-
-];
+    Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+    Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+    Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+    Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+  ];
+  
 World.add(world, walls);
 World.add(world, Bodies.rectangle(200, 200, 50, 50));
