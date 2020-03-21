bcbd5d1b171bed22ff815e08d1efcd672a3e87a8
ThienStylemen <thienstylemen@gmail.com>
2020-03-21 18:44:46 +0700

    Grid Generation and try .map function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies} = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
 const width = 600;
 const height = 600;
@@ -24,6 +24,27 @@ const walls = [
     Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
     Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
     Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
-  ];
-  
+];
+
 World.add(world, walls);
+
+// Maze generation
+
+// map: Calls a defined callback function on each element of an array, 
+//and returns an array that contains the results.
+
+const grid = Array(3)
+    .fill(null)
+    .map(() =>  Array(3).fill(false)); //we're going to generate a brand new and different array
+console.log(grid);
+grid[0].push(true); // console.log before and after this line have the same result!!!
+console.log(grid);
+
+/*we are creating one single array and then throwing it in at every location in
+So in memory there's only one in the array that we made a change to this in array would affect every index
+*/
+const grid2 = Array(3)
+    .fill([false,false,false]);
+console.log(grid2);
+grid2[0].push(true);// console.log before and after this line have the same result!!!
+console.log(grid2);
