c5acb22d78b5e73b0ed3b7f34e233b1064ccd956
ThienStylemen <thienstylemen@gmail.com>
2020-03-22 14:01:15 +0700

    Determining Movement Direction

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const cells =3;
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -33,10 +33,10 @@ World.add(world, walls);
 
 // map: Calls a defined callback function on each element of an array, 
 //and returns an array that contains the results.
- 
+
 const grid = Array(cells)
     .fill(null)
-    .map(() =>  Array(cells).fill(false)); //we're going to generate a brand new and different array
+    .map(() => Array(cells).fill(false)); //we're going to generate a brand new and different array
 
 console.log(grid);
 grid[0].push(true); // console.log before and after this line have the same result!!!
@@ -44,40 +44,61 @@ console.log(grid);
 
 const verticals = Array(cells)
     .fill(null)
-    .map(()=> Array(cells-1).fill(false));
+    .map(() => Array(cells - 1).fill(false));
 
-const horizontals = Array(cells-1)
+const horizontals = Array(cells - 1)
     .fill(null)
-    .map( ()=> Array(cells).fill(false));
+    .map(() => Array(cells).fill(false));
 
 // console.log(verticals);
 // console.log(horizontals);
 
-const startRow = Math.floor(Math.random()*cells);
-const startColumn = Math.floor(Math.random()*cells);
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
 
-const stepThroughCell = (row,col)=>{    // recursive func
+const shuffle = (arr) => {
+    let counter = arr.length;
+
+    while (counter > 0) {
+
+        let index = Math.floor(Math.random() * counter);
+        counter--;
+
+        //swap
+        temp = arr[index];
+        arr[index] = arr[counter];
+        arr[counter] = temp;
+    }
+    return arr;
+
+}
+
+const stepThroughCell = (row, col) => {    // recursive func
     // if i have visited at cells [row,col], then return
-    if (grid[row][col] ===true) return;
+    if (grid[row][col] === true) return;
     // Mark this cells as being visited
     grid[row][col] = true;
     // Assemble randomly-ordered list of neighbors
-    const neighbors =[
-        [row-1,col],
-        [row,col+1],
-        [row+1, col],
-        [row,col-1]
-    ];
+    const neighbors = shuffle([
+        [row - 1, col, 'up'],
+        [row, col + 1, 'right'],
+        [row + 1, col, 'down'] ,
+        [row, col - 1, 'left']
+    ]);
     // For each neighbor ...
+    for (let neighbor of neighbors) {
+        const [nextRow, nextCol, direction] = neighbor;
+        //see if that neighbors is out of bounds
+        if (nextRow < 0 || nextRow >= cells || nextCol < 0 || nextCol >= cells) 
+            continue;
+        // if we have visited that neighbor, continue to the next 
+        if (grid[nextRow][nextCol])
+            continue;
+        // remove a wall from either horizontals or vericals
 
-    //see if that neighbors is out of bounds
-
-    // if we have visited that neighbor, continue to the next 
-
-    // remove a wall from either horizontals or vericals
-
+    }
     // visit that next cell
 }
 
-stepThroughCell(startRow, startColumn);
-console.log(grid);
+stepThroughCell(1, 1);
+// console.log(grid);
